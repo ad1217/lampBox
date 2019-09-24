6f86f11c8fc7f9ba0a7eead6aa0265b3eabda277
Adam Goldsmith <adam@adamgoldsmith.name>
2019-09-24 17:01:23 -0400

    Fix namespace names in SVG

--- splitSVG.py
+++ splitSVG.py
@@ -3,9 +3,16 @@ import copy
 import os
 import sys
 
-def split_svg(filename):
+# from https://stackoverflow.com/questions/54439309/how-to-preserve-namespaces-when-parsing-xml-via-elementtree-in-python
+def register_all_namespaces(filename):
+    namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
+    for ns in namespaces:
+        ET.register_namespace(ns, namespaces[ns])
 
-    tree = ET.parse(sys.argv[1])
+def split_svg(filename):
+    register_all_namespaces(filename)
+
+    tree = ET.parse(filename)
     root = tree.getroot()
     layers = [g.get('{http://www.inkscape.org/namespaces/inkscape}label')
               for g in root.findall('{http://www.w3.org/2000/svg}g')]
